aec3ab96eef5e633107605fb2809eddf958370f1
Tomatosky <2302849721@qq.com>
2022-04-18 20:29:52 +0800

    no message

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "建立索引"
-/usr/local/sphinx-jieba/bin/indexer -c /usr/local/sphinx-jieba/sphinx.conf  --all -–rotate
+/usr/local/sphinx-jieba/bin/indexer -c /usr/local/sphinx-jieba/sphinx.conf  --all --rotate
 
 echo "启动服务"
 /usr/local/sphinx-jieba/bin/searchd -c /usr/local/sphinx-jieba/sphinx.conf
